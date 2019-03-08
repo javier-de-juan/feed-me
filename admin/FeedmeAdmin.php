0e6964f9082825ffdd33a1bb833036d8b1b98741
Javier De Juan <social@javierdejuan.es>
2019-03-08 11:19:07 +0100

    feat: Create plugin using default's behaviour from boilerplate and allowing WordPress to activate/deactivate the plugin

---
+++ admin/FeedmeAdmin.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ *
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/admin
+ */
+
+namespace FeedMe\admin;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/admin
+ * @author     Javier De Juan Trujillo social@javierdejuan.es
+ */
+class FeedmeAdmin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $plugin_name The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $version The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param      string $plugin_name The name of this plugin.
+	 * @param      string $version     The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles(): void {
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/feed-me-admin.css', array(), $this->version, 'all' );
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts(): void {
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/feed-me-admin.js', array( 'jquery' ), $this->version, false );
+	}
+
+}
---
+++ core/FeedmeDeactivator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ *
+ * @since      1.0.0
+ *
+ * @package    Feed_me
+ * @subpackage Feed_me/core
+ */
+
+namespace FeedMe\core;
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Feed_me
+ * @subpackage Feed_me/core
+ * @author     Javier De Juan Trujillo social@javierdejuan.es
+ */
+class FeedmeDeactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
